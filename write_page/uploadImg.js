ecdfa4e35218408ed6d2813c1c90240e836c686e
19127618 <91797598+19127618@users.noreply.github.com>
2021-11-13 23:14:09 +0700

    post editor: +header + sidebar

--- write_page/uploadImg.js
+++ write_page/uploadImg.js
@@ -4,13 +4,13 @@ let numOfFile = document.getElementById("numOfFile");
 
 function preview(){
   imgContainer.innerHTML = '';
-  numOfFile.textContent = fileInput.files.length + ' file selected';
+  //numOfFile.textContent = fileInput.files.length + ' file selected';
 
   for(i of fileInput.files){
     let reader = new FileReader();
     let figure = document.createElement('figure');
     // figure.classList.add("col-12")
-    figure.classList.add("col-md-2")
+    //figure.classList.add("col-md-12")
     let figCap = document.createElement("figcaption");
     //figCap.innerText = i;
     figure.appendChild(figCap);
@@ -24,3 +24,10 @@ function preview(){
     reader.readAsDataURL(i);
   }
 }
+
+$(".p-title-input").each(function () {
+  this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+}).on("input", function () {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+});
