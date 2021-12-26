2daaf498227fead4624e0d806467fda123bcac13
19127618 <91797598+19127618@users.noreply.github.com>
2021-12-26 23:35:31 +0700

    home -> run

---
+++ root/controller/blogController.js
@@ -0,0 +1,20 @@
+import blogsModel from "../models/blogs.model.js";
+import mongooseObject from '../ulti/mongoose.js'
+
+class blogController{
+    async index(req, res, next){
+        blogsModel.find({})
+            .then( blog => {
+                res.render('home', {
+                    blog: mongooseObject.multipleMongooseToObject(blog),
+                    user: req.user
+                },
+                console.log(blog)
+                )}
+            )
+            .catch(error => next(error));
+
+    }
+}
+
+export default new blogController();
--- root/middlewares/routes.mdw.js
+++ root/middlewares/routes.mdw.js
@@ -1,12 +1,14 @@
 import accountRoute from '../routes/account.route.js';
 import blogRoute from '../routes/blog.route.js';
 import {upload, uploadImg} from '../controller/uploadImgController.js'
-
+import blog from '../controller/blogController.js';
 
 export default function (app) {
-    app.get('/', function (req, res) {
-       res.render('home')
-    });
+    // app.get('/', function (req, res) {
+    //    res.render('home')
+    // });
+    app.get('/', blog.index);
+
 
     app.use('/account', accountRoute);
     app.use('/blog', blogRoute);
